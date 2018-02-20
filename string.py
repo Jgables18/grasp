25436ae09d93c84a5a32472ba6827256e08a8170
jgables18 <31707093+Jgables18@users.noreply.github.com>
2018-02-20 13:39:12 -0600

    BB
      BB

--- string.py
+++ string.py
@@ -3,18 +3,34 @@ RPL.RoboPiInit("/dev/ttyAMA0",115200)
 import sys, tty, termios, signal
 
 # Definitions
+# pin 1 = left
+#500-1190
+
+# pin 2 = right
+#1810
+-2500
+
 def stopAll():
     pass
 
 def open():
-  #RPL.servoWrite(0, 500)
-  #RPL.servoWrite(1, 2500)
+  RPL.servoWrite(0, 500)
+  RPL.servoWrite(1, 2500)
   print "open"
 
 def close():
-  #RPL.servoWrite(0, 950)
-  #RPL.servoWrite(1, 1940)
+  RPL.servoWrite(0, 1000)
+  RPL.servoWrite(1, 2000)
   print "close"
+
+i = 500
+j = 2500
+
+def stepclose():
+    RPL.servoWrite(0,i+10)
+    RPL.servoWrite(1,j-10)
+    print "step close"
+
 
 fd = sys.stdin.fileno()
 old_settings = termios.tcgetattr(fd)
@@ -44,5 +60,7 @@ while True:
       open()
     elif ch == "a":
       close()
+    elif ch == "s":
+      stepclose()
     else:
       stopAll()
